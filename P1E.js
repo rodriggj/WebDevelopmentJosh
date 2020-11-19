0682022c8e001ba0705e62ecdea48f15c993e3ce
Joshua Rodriguez <joshuarodriguez@Joshuas-MacBook-Air.local>
2020-11-19 00:11:29 -0600

    love

--- P1E.js
+++ P1E.js
@@ -2,6 +2,6 @@ let title = "Shopping To Do List";
 title.toUpperCase();
 let list = ['Get whips', 'Get chains', 'Get babies'];
 const creator = 'Joshua Rodriguez';
-console.log.toUpperCase(title)
-console.log(list)
-console.log('Joshua Rodriguez')
+console.log.toUpperCase(title);
+console.log(list);
+console.log(creator);
