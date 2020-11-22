c24e00ad2bca3c1169565cacd35a4b4563cef3b0
Joshua Rodriguez <joshuarodriguez@Joshuas-MacBook-Air.local>
2020-11-22 16:00:16 -0600

    added loop for list

--- P1E.js
+++ P1E.js
@@ -2,6 +2,12 @@ let title = "Shopping To Do List";
 title.toUpperCase();
 let list = ['Get whips', 'Get chains', 'Get babies'];
 const creator = 'Joshua Rodriguez';
-console.log.(title.toUpperCase());
-console.log(list);
+console.log(title.toUpperCase());
+console.log(list[0]);
+console.log(list[1]);
+console.log(list[2]);
 console.log(creator);
+// loop for the list
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+}
